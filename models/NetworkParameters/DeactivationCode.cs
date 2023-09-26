e712201a7bca8677fd03d0437dec03a489262780
alexeysp11 <alexeyspiridon1111@gmail.com>
2023-09-26 22:50:06 +0500

    edited docs

--- models/NetworkParameters/DeactivationCode.cs
+++ models/NetworkParameters/DeactivationCode.cs
@@ -2,8 +2,8 @@ namespace WokflowLib.Authentication.Models.NetworkParameters;
 
 public class DeactivationCode
 {
-    public string DeactivationGuid { get; set; }
-    public string Code { get; set; }
+    public string? DeactivationGuid { get; set; }
+    public string? Code { get; set; }
     public System.DateTime CodeSendingDt { get; set; }
-    public string ExceptionMessage { get; set; }
+    public string? ExceptionMessage { get; set; }
 }
--- models/NetworkParameters/UserCreationResult.cs
+++ models/NetworkParameters/UserCreationResult.cs
@@ -3,8 +3,8 @@ namespace WokflowLib.Authentication.Models.NetworkParameters;
 public class UserCreationResult
 {
     public bool IsUserAdded { get; set; }
-    public string SignUpGuid { get; set; }
-    public string VerificationCode { get; set; }
+    public string? SignUpGuid { get; set; }
+    public string? VerificationCode { get; set; }
     public System.DateTime CodeSendingDt { get; set; }
-    public string ExceptionMessage { get; set; }
+    public string? ExceptionMessage { get; set; }
 }
